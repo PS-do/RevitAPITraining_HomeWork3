3c87087c036610f85a59812da3dd5af3ebda50e6
Сергей Плюхин <psg.pgs@mail.ru>
2023-02-05 20:50:18 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITraining_task3_1/Main.cs
@@ -0,0 +1,41 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITraining_task3_1
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+
+            IList<Reference> selectedElementRefList = uidoc.Selection.PickObjects(ObjectType.Edge, new WallFilter(), "Выберите стены");
+            double sumVolume = 0;
+            foreach (var selectedElement in selectedElementRefList)
+            {
+                Wall oWall = doc.GetElement(selectedElement) as Wall;
+                if (oWall != null)
+                {
+                sumVolume += UnitUtils.ConvertFromInternalUnits(
+                        oWall.get_Parameter(BuiltInParameter.HOST_VOLUME_COMPUTED).AsDouble()
+                        , UnitTypeId.CubicMeters);
+                }
+
+
+            }
+            TaskDialog.Show("Объм стен", $"Суммарный объём выбранных стен: {sumVolume} ");
+            return Result.Succeeded;
+        }
+    }
+}
