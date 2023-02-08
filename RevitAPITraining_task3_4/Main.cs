7cc22ea6a75704df0e69c08cdc4b269e47274418
Сергей Плюхин <psg.pgs@mail.ru>
2023-02-08 19:23:07 +0300

    ed

---
+++ RevitAPITraining_task3_4/Main.cs
@@ -0,0 +1,54 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITraining_task3_4
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Создайте параметр проекта экземпляра «Наименование» с типом данных «Текст» для труб.
+            //Создайте приложение, которое для всех труб в модели записывает значение в
+            //созданный параметр в следующую формате «Труба НАРУЖНЫЙ_ДИАМЕТР / ВНУТРЕННИЙ_ДИАМЕТР»,
+            //где НАРУЖНЫЙ_ДИАМЕТР и ВНУТРЕННИЙ_ДИАМЕТР соответствующие диаметры трубы в
+            //миллиметрах.
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<Pipe> pipes = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_PipeCurves)
+                .WhereElementIsNotElementType()
+                .Cast<Pipe>()
+                .ToList();
+            foreach (Pipe pipe in pipes)
+            {
+                if (pipe != null)
+                {
+                    double OUTER_DIAMETER = UnitUtils.ConvertFromInternalUnits(//НАРУЖНЫЙ_ДИАМЕТР
+                            pipe.get_Parameter(BuiltInParameter.RBS_PIPE_OUTER_DIAMETER).AsDouble(),
+                            UnitTypeId.Millimeters);
+                    double INNER_DIAM_PARAM = UnitUtils.ConvertFromInternalUnits(// ВНУТРЕННИЙ_ДИАМЕТР
+                            pipe.get_Parameter(BuiltInParameter.RBS_PIPE_INNER_DIAM_PARAM).AsDouble(),
+                            UnitTypeId.Millimeters);
+                    string paramStr = "Труба " + OUTER_DIAMETER.ToString(".#") + "/" + INNER_DIAM_PARAM.ToString(".#");
+                    using (Transaction ts = new Transaction(doc, "Set parametrs"))
+                    {
+                        ts.Start();
+                        pipe.LookupParameter("Наименование").Set(paramStr);
+                        ts.Commit();
+                    }
+                }
+            }
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ RevitAPITraining_task3_4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("RevitAPITraining_task3_4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RevitAPITraining_task3_4")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("a9ee9fce-5b8c-4352-b2c0-e4924c12f2b4")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
